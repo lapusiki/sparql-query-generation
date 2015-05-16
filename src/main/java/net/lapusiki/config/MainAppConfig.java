175923da0cc84c357192c4c8f5da1c33c38f23c9
blvp <blvp.me@gmail.com>
2015-05-16 22:21:52 +0300

    query builder refactored

--- src/main/java/net/lapusiki/config/MainAppConfig.java
+++ src/main/java/net/lapusiki/config/MainAppConfig.java
@@ -2,9 +2,15 @@ package net.lapusiki.config;
 
 import info.debatty.java.stringsimilarity.Damerau;
 import info.debatty.java.stringsimilarity.StringSimilarityInterface;
+import net.lapusiki.core.QueryBuilder;
+import net.lapusiki.core.service.VariableGenerator;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import javax.xml.ws.handler.MessageContext;
 
 /**
  * Created by blvp on 16.05.15.
